705342b653e800f74575c6d50e85133d885efa81
James Miller <james.rex.miller.4@gmail.com>
2019-12-11 09:29:05 -0700

    Import Form and Container components

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './Form/Form';
-import Reservations from './Reservations/Reservations';
-
+import Form from '../Form/Form';
+import Reservations from '../Reservations/Reservations';
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-          <
+          <Form />
         </div>
         <div className='resy-container'>
-          
+          <Reservations />
         </div>
       </div>
     )
