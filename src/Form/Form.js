f7e63a1db28bb2c9f420d07378abe9f8b78a6c18
James Miller <james.rex.miller.4@gmail.com>
2019-12-11 09:35:01 -0700

    Add Form setup/boilerplate

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -3,8 +3,22 @@ class Form extends Component {
     super(props);
     this.state = {  }
   }
+
+
+  submitReservation() {
+
+  }
   render() { 
-    return (  );
+    return (
+      <div className='form-wrapper-div'>
+        <h1>I AM A FORM</h1>
+        <input className="form-input" type='text' name="name" placeholder="Name"></input>
+        <input className="form-input" type='date' name="date" placeholder="Date(mm/dd)"></input>
+        <input className="form-input" type='text' name="time" placeholder="Time"></input>
+        <input className="form-input" type='number' name="guests" placeholder="Number of Guests"></input>
+        <button onclick="form-input" className='submit btn'>Make Reservation</button>
+      </div>
+      );
   }
 }
  
