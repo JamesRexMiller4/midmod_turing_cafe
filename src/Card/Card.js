3dd75317841a9bef523672b5dfafe2b15426cb35
James Miller <james.rex.miller.4@gmail.com>
2019-12-11 09:35:22 -0700

    Add cancel button to Card boilerplate

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -5,6 +5,7 @@ const Card = ({ name, date, time, guests}) => {
       <h3>{date}</h3>
       <h3>{time}</h3>
       <h3>Number of Guests: {guests}</h3>
+      <button className="cancel btn">Cancel</button>
     </div>
    );
 }
