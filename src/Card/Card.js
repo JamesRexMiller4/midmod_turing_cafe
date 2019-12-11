ccffbcfbe07d5b5da51454e7abf4844dee1e7d70
James Miller <james.rex.miller.4@gmail.com>
2019-12-11 09:51:41 -0700

    Add Card styles + update proptype + update names for props

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import './Card.css';
 
-
-const Card = ({ name, date, time, guests}) => {
+const Card = ({ name, date, time, number}) => {
   return ( 
-    <div>
+    <div className="card-wrapper-div">
       <h2>{name}</h2>
       <h3>{date}</h3>
       <h3>{time}</h3>
-      <h3>Number of Guests: {guests}</h3>
+      <h3>Number of Guests: {number}</h3>
       <button className="cancel btn">Cancel</button>
     </div>
    );
@@ -18,10 +18,9 @@ export default Card;
 
 
 Card.propTypes = {
-
+  id: PropTypes.number,
   name:PropTypes.string,
-  date:PropTypes.number,
-  time:PropTypes.number,
-  guests:PropTypes.number
-
+  date:PropTypes.string,
+  time:PropTypes.string,
+  number:PropTypes.number
 }
